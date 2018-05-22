8efb41ef27c58f9323c08fad205fd35214581d74
Aquib Master <aquibmaster@gmail.com>
2018-05-22 21:48:35 +1200

    Implement lat long scrape

--- scraper.js
+++ scraper.js
@@ -16,9 +16,36 @@ const scrapeRegion = $ => {
     }
 }
 
-const scrapeLatLongs = $ => {}
+const scrapeLatLongs = $ => {
+    const mapInfo = $(
+        'div#p_lt_ctl08_wpPlaceholder_p_lt_ctl02_Lumino_PracticeSearchResultWidget_pnlGoogleMaps',
+    )
+        .html()
+        .trim()
 
-const scrapePractices = $ => {}
+    const regex = /addGoogleMarker\(p_lt_ctl08_wpPlaceholder_p_lt_ctl02_Lumino_PracticeSearchResultWidget_map, (-?\d*.\d*), (-?\d*.\d*), '(.*?)'/gm
+
+    const latLongs = {}
+
+    while ((m = regex.exec(mapInfo)) !== null) {
+        // This is necessary to avoid infinite loops with zero-width matches
+        if (m.index === regex.lastIndex) {
+            regex.lastIndex++
+        }
+
+        // The result can be accessed through the `m`-variable.
+        latLongs[m[3]] = {
+            latitude: parseFloat(m[1]),
+            longitude: parseFloat(m[2]),
+        }
+    }
+
+    return latLongs
+}
+
+const scrapePractices = $ => {
+    return 'kek'
+}
 
 exports.scrape = html => {
     if (!html) throw Error('no html')
@@ -44,6 +71,11 @@ exports.scrape = html => {
             region,
             phoneNumber: '+64 9-373 4962',
             emailAddress: 'proudmouth@lumino.co.nz',
+        },
+        {
+            name: 'debug',
+            latLongs,
+            practices,
         },
     ]
 }
